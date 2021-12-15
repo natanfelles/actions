4d635aab501c5187fbcab3a78cc8dad30e224fd1
Natan Felles <natanfelles@gmail.com>
2021-12-15 06:15:19 -0300

    Fix CS

--- src/Foo.php
+++ src/Foo.php
@@ -1,13 +1,20 @@
 <?php
-
+/*
+ * This file is part of Actions Test Project.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 class Foo
 {
     public function bar($var)
     {
-        if($var === 300) {
+        if ($var === 300) {
             return 'Sparta!';
         }
-        if(false) {
+        if (false) {
             return 'Mamamia';
         }
         return 'Xurupita';
--- tests/FooTest.php
+++ tests/FooTest.php
@@ -1,10 +1,17 @@
 <?php
-
+/*
+ * This file is part of Actions Test Project.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 use PHPUnit\Framework\TestCase;
 
 class FooTest extends TestCase
 {
-    public function testBar()
+    public function testBar() : void
     {
         $foo = new Foo();
         self::assertSame('Xurupita', $foo->bar(24));
